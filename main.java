448cf92b8f12705f0b1af47e9f71b6c06979f8da
Adriel <adriel.chagay@gmail.com>
2020-04-24 17:04:34 -0300

    Se añadió la clase Buffer

---
+++ main.java
@@ -0,0 +1,39 @@
+public class Main {
+
+	public static final int CANT_PROD = 5;
+	public static final int CANT_CONS = 3;
+	
+	
+	public static void main(String[] args) {
+		
+		Buffer buffer [] = new Buffer();						//?¿ esto no se si deberia ser un array ¿?
+		
+		Thread productores [] = new Thread [CANT_PROD];			//creo array de hilos que contendran objetos del tipo Productor
+		Thread consumidores [] = new Thread [CANT_CONS];		//creo array de hilos que contendran objetos del tipo Consumidor
+		
+		
+		for (int i=0; i<CANT_PROD; i++) {
+			
+			Productor productor = new Productor(buffer);			//creo un objeto productor
+			productores[i] = new Thread(productor);					//seteo el hilo con el objeto productor
+			productores[i].setName("Productor " + i);				//declaro nombres para los hilos
+			productores[i].start();									//ejecuto el hilo
+		}
+
+		
+		for (int i=0; i<CANT_CONS; i++) {
+			
+			Consumidor consumidor = new Consumidor(buffer);
+			consumidores[i] = new Thread(consumidor);
+			consumidores[i].setName("Consumidor " + i);
+			consumidores[i].start();
+		}
+		
+		
+		Log log = new Log(buffer, consumidores);					//creo un objeto log que contiene la info de los resultados
+
+	}
+
+}
+
+
