4b16d2f36b38c0ae87fc76913e5f3000bcf498b1
Adriel <adriel.chagay@gmail.com>
2020-04-24 22:51:25 -0300

    Prueba de semaforo

--- src/tp1/Buffardo.java
+++ src/tp1/Buffardo.java
@@ -1,42 +1,61 @@
 package tp1;
 
+import java.util.concurrent.Semaphore;
+
 public class Buffardo {
     private Article[] lugares;
+    private final Semaphore semaforo;
     int cantLugares;
 
     public Buffardo(int i) {
         cantLugares = i;
         lugares = new Article[cantLugares];
+        semaforo = new Semaphore(1);
     }
     
     public void addItem(Article art) {
-    	
-    	if( !this.isFull() ){
-    		for (int i = 0; i < this.lugares.length; i++) {
-                if(this.lugares[i]==null){
-                	this.lugares[i]=art;
-                    System.out.println("Se agregó el articulo exitosamente");
-                    i=cantLugares+1;
-                }
-            }
-        }else{
-            System.out.println("Jodete puto, no entra");
-        }
+    	try {
+    		semaforo.acquire();
+    		//TODO duracion en ms
+    		if( !this.isFull() ){
+    			for (int i = 0; i < this.lugares.length; i++) {
+    				if(this.lugares[i]==null){
+    					this.lugares[i]=art;
+    					System.out.println("Se agregó el articulo exitosamente");
+    					i=cantLugares+1;
+    				}
+    			}
+    		}else{
+    			System.out.println("Jodete puto, no entra");
+    		}
+    	}catch(InterruptedException e){
+    		e.printStackTrace();
+    	}finally {
+    		semaforo.release();
+    	}
     }
 
 
     public void takeItem() {
-        if( !this.isEmpty() ) {
-        	for (int i = 0; i < this.lugares.length; i++) {
-        		if(this.lugares[i]!=null){
-        			this.lugares[i] = null;
-        			System.out.println("Se consumio el articulo exitosamente");
-        			i=this.lugares.length+1;
-        		}
-            }
-        }else{
-            System.out.println("No hay articulos, aguanta un cacho");
-        }
+    	try {
+    		semaforo.acquire();
+    		//TODO duracion en ms
+    		if( !this.isEmpty() ) {
+    			for (int i = 0; i < this.lugares.length; i++) {
+    				if(this.lugares[i]!=null){
+    					this.lugares[i] = null;
+    					System.out.println("Se consumio el articulo exitosamente");
+    					i=this.lugares.length+1;
+    				}
+    			}
+    		}else{
+    			System.out.println("No hay articulos, aguanta un cacho");
+    		}
+    	}catch(InterruptedException e){
+    		e.printStackTrace();
+    	}finally {
+    		semaforo.release();
+    	}
     }
 
     public boolean isFull(){
